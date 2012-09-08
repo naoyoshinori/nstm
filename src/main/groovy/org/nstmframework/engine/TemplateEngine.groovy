b755f7f5dd832c10b965dab39b7946199a2f1d02
yoshi-naoyuki <yoshi.nao.you.key@gmail.com>
2012-09-08 12:27:02 +0900

    テンプレートエンジン更新

--- src/main/groovy/org/nstmframework/engine/TemplateEngine.groovy
+++ src/main/groovy/org/nstmframework/engine/TemplateEngine.groovy
@@ -24,56 +24,53 @@ abstract class TemplateEngine {
 
     boolean debug
 
-    def createFile(String src, String dest) {
-        def src_file = new File(template_root, src)
-        def dest_file = new File(app_root, dest)
+    def createFile(String src_relative_path, String dest_relative_path) {
+        def src_file = new File(template_root, src_relative_path)
+        def dest_file = new File(app_root, dest_relative_path)
 
-        println "  create  " + debug ? dest_file.canonicalPath : dest
+        println "  create  " + (debug ? dest_file.canonicalPath : dest_relative_path)
 
-        def engine = new GStringTemplateEngine()
-        def template = engine.createTemplate(src_file).make(binding)
-        FileUtils.writeStringToFile(dest_file, template.toString())
+        def generated_from_template = new GStringTemplateEngine().createTemplate(src_file).make(binding).toString()
+        FileUtils.writeStringToFile(dest_file, generated_from_template)
     }
 
-    def copyFile(String src, String dest) {
-        def src_file = new File(template_root, src)
-        def dest_file = new File(app_root, dest)
+    def copyFile(String src_relative_path, String dest_relative_path) {
+        def src_file = new File(template_root, src_relative_path)
+        def dest_file = new File(app_root, dest_relative_path)
 
-        println "  create  " + debug ? dest_file.canonicalPath : dest
+        println "  create  " + (debug ? dest_file.canonicalPath : dest_relative_path)
 
         FileUtils.copyFile(src_file, dest_file)
     }
 
-    def createDirectory(String path) {
-        def path_file = new File(app_root, path)
+    def createDirectory(String relative_path) {
+        def path_file = new File(app_root, relative_path)
 
-        println "  create  " + debug ? path_file.canonicalPath : path
+        println "  create  " + (debug ? path_file.canonicalPath : relative_path)
 
         FileUtils.forceMkdir(path_file)
     }
 
     def start() {
-        def subclass = this.class
-
         if (!template_root) {
-            def nstm_home = new File(subclass.protectionDomain.codeSource.location.path).parent
+            // set $NSTM_HOME/templates
+            def nstm_home = new File(TemplateEngine.class.protectionDomain.codeSource.location.path).parent
             template_root = new File(nstm_home, "templates").canonicalPath
         }
 
         if (!app_root) {
+            // set current directory
             app_root = "."
         }
 
-        def subclass_fields = subclass.declaredFields
+        def subclass_fields = this.class.declaredFields
         subclass_fields.each {
             if (it.isAnnotationPresent(TemplateParameter)) {
-                def pd = new PropertyDescriptor(it.name, subclass)
-                def val = pd.readMethod.invoke(this)
-                binding["${it.name}"] = val
+                binding["${it.name}"] = new PropertyDescriptor(it.name, this.class).readMethod?.invoke(this)
             }
         }
 
-        def subclass_methods = subclass.methods
+        def subclass_methods = this.class.methods
         subclass_methods.each {
             if (it.isAnnotationPresent(TemplateGenerator)) {
                 it.invoke(this)
